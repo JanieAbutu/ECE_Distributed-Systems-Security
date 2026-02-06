768a3f4215d0836cde66fb188c958457d4c341de
JanieAbutu <janieabutu@gmail.com>
2026-02-06 09:17:56 +0100

    Updated Project

---
+++ Projects/Microservices/Container_POMicroservices/frontend_service/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const path = require("path"); 
+const app = express();
+
+const PORT = process.env.PORT_ONE || 8080;
+
+app.use(express.json());
+
+// Serve index.html on root
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`frontend-Service running at http://localhost:${PORT}`);
+});
---
+++ Projects/Microservices/Container_POMicroservices/product_service/modelProduct.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose"); const ProductSchema = mongoose.Schema({
+name: String,
+description: String, price: Number, created_at: {
+type: Date,
+default: Date.now(),
+},
+});
+module.exports = Product = mongoose.model("product", ProductSchema);
